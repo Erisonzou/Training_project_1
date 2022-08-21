d99d6b7cdeb2fbebbd1e7ed69f51dafd8e8dbf92
Erisonzou <zou.ericson123@gmail.com>
2022-08-21 17:02:33 -0400

    updated code

--- src/project_1/fileFuntions.java
+++ src/project_1/fileFuntions.java
@@ -1,5 +1,114 @@
 package project_1;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class fileFuntions {
+	
+	 ArrayList<String> Files = new ArrayList<String> (); 
+	 
+	 	void Addfile() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the File you would like to add");
+		
+		try{
+			String current_file = scan.nextLine();
+			if (this.Files.contains(current_file)) {
+				System.out.println("File is already in the System");
+			}else
+			{
+			this.Files.add(current_file);
+			System.out.println("*** File Added ***");
+			}}
+		catch(Exception e) {
+				System.out.println("Please enter a File_name");
+				Addfile();
+			}
+		AddAgain();
+	}
+		
+		void AddAgain() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Would you like to add more Files?\n1.Yes\n2.No");
+		try {
+			int choice  = scan.nextInt();
+			if ( choice == 1) {
+				Addfile();
+			}
+		}catch(Exception e) {
+			System.out.println("Input either 1 or 2");
+			AddAgain();
+		}
+		}
+	
+		void DeleteFiles() {
+		Scanner scan = new Scanner(System.in);
+		try {
+			System.out.print("What file would you like to delete");
+			String Deletion_File = scan.nextLine();
+		
+		 if (this.Files.contains(Deletion_File))
+		 {
+			 this.Files.remove(Deletion_File);
+			 System.out.println("** Files found and Deleted **");
+		 }
+		 else
+		 {
+			 System.out.println("File not Found");
+		 }}catch(Exception e) {
+			 System.out.println("Please enter a file name");
+			 DeleteFiles();
+		 }
+		DeleteAgain();
+	 }
+		
+		void DeleteAgain() {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Would you like to Delete more Files?\n1.Yes\n2.No");
+			try {
+				int choice  = scan.nextInt();
+				if ( choice == 1) {
+					DeleteFiles();
+				}
+			}catch(Exception e) {
+				System.out.println("Input either 1 or 2");
+				DeleteAgain();
+			}
+			}
+	 
+	  void SearchFile () {
+			Scanner scan = new Scanner(System.in);
+			try {
+				System.out.print("What file would you like to find");
+				String Searching_File= scan.nextLine();
+				
+			 if (this.Files.contains(Searching_File)) {
+				 
+				 System.out.println("File Found");
+			 }
+			 else
+			 {
+				 System.out.println("There is no such File");
+			 }} catch(Exception e) {
+				 System.out.println("Exeception Caught in SearchFile");
+				 
+			 }
+		 }
+	 
+	  void file_names() {
+			
+			if (this.Files.isEmpty()) {
+				System.out.println("There are currently no files in the System");
+			}else {
+				System.out.println("these are the current files in the System.");
+				System.out.println("------------------------------------------");
+				for (String x:this.Files) {
+					System.out.println(x);
+				}
+				System.out.println("------------------------------------------");
+			}
+
+		}
+	
 
 }
--- src/project_1/main.java
+++ src/project_1/main.java
@@ -1,5 +1,87 @@
 package project_1;
 
-public class main {
+import java.util.*;
+import java.io.*;
+
+public class main extends fileFuntions{
+
+	 
+	 private void ExecuteChoice() {
+		 Scanner scan = new Scanner(System.in);
+		 
+		 try {
+			 System.out.println(" 1. Add File \n 2. Delete File \n 3. Find File \n 4. Main menu");
+			 int choice = scan.nextInt();
+			 if (choice == 1) {
+				this.Addfile();
+			 }else if (choice == 2) {
+				this.DeleteFiles();
+			 }else if (choice == 3) {
+				 this.SearchFile();
+			 }else if (choice == 4) {
+				 System.out.println("Returning to Main Menu");
+				 runMachine();
+			 }else {
+				 System.out.println("Please Select one of the Three Choices");
+				 ExecuteChoice();
+			 }
+			 
+		 }catch(Exception e) {
+			 System.out.println("Please Select one of the Three Choices");
+			 ExecuteChoice();
+		 }
+	 }
+
+	 void runMachine() {
+		
+		boolean running =true;
+		Scanner scan = new Scanner(System.in);
+		
+		while (running == true)
+		{
+			try
+			{
+				System.out.println("------------------------------------------------------------------");
+				System.out.println("LockedMe.Com");
+				System.out.println("Erison Zou\r");
+				System.out.println("Main Menu");
+				System.out.println("Hi what would you like to do today\r");
+				System.out.println(" 1. Show all current Files\n 2. User Interface\n 3. End Application");
+				System.out.println("------------------------------------------------------------------");
+				int choice =  scan.nextInt();
+				
+				if (choice == 1) {
+					this.file_names();
+				}else if (choice == 2) {
+					this.ExecuteChoice();
+					Collections.sort(this.Files);
+				}else if (choice == 3) {
+					System.out.println("Thank you for using this application!!");
+					System.out.println("System is exiting");
+					running = false;
+				}else {
+					System.out.println("Please enter a number between the range 1-3 to signify your choice: ");
+					runMachine();
+					
+				}
+				
+			}catch (Exception e) {
+				
+				System.out.println("Please enter a numeric input between the range 1-3 to signify your choice:");
+				runMachine();
+				
+			}
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+
+		main current_files =  new main();
+		
+		current_files.runMachine();
+			
+	}
 
 }
+
